01c3850056c1c2b6e31e21c493bc5f2039937e28
zxj5470 <zxj5470@foxmail.com>
2018-06-01 06:00:48 -0400

    Fri Jun  1 06:00:48 EDT 2018

--- wx.d.ts
+++ wx.d.ts
@@ -1,4 +1,4 @@
-// generate time:2018-5-31 06:00:24 
+// generate time:2018-6-1 06:00:46 
 // Type definitions for wx app
 // Definitions by: hellopao <https://github.com/hellopao/wx.d.ts>
 // Update by: zxj5470 <https://github.com/zxj5470/wx.d.ts>
