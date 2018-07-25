b2c68b44060f981fa137042e9090ae75737d7c50
zxj5470 <zxj5470@foxmail.com>
2018-07-25 06:00:31 -0400

    Wed Jul 25 06:00:31 EDT 2018

--- wxPromise.d.ts
+++ wxPromise.d.ts
@@ -1,4 +1,4 @@
-// generate time:2018-7-24 09:43:46 
+// generate time:2018-7-25 06:00:24 
 // Type definitions for wx app
 // Definitions by: hellopao <https://github.com/hellopao/wx.d.ts>
 // Update by: zxj5470 <https://github.com/zxj5470/wx.d.ts>
